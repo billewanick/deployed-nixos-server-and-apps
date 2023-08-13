53973e8e5eef51ad9be695fe82d7f220242e6fca
Bill Ewanick <bill@ewanick.com>
2023-08-13 10:15:43 -0400

    Call my flakes remotely

--- nixos-apps/cutesealfanpage.love.nix
+++ nixos-apps/cutesealfanpage.love.nix
@@ -22,7 +22,7 @@ in
       enable = true;
       description = "The hakyll executable that rebuilds the site when a new blog post is created.";
       serviceConfig = {
-        ExecStart = "${pkgs.nix}/bin/nix run .#hakyll-site -- watch --no-server";
+        ExecStart = "nix run git+https://github.com/billewanick/seal-blog.git#hakyll-site -- watch --no-server";
       };
     };
 
@@ -31,7 +31,7 @@ in
       description = "The haskell script that creates the new post of the day.";
       startAt = "08:12:42";
       serviceConfig = {
-        ExecStart = "${pkgs.nix}/bin/nix run .#generateSealPosts";
+        ExecStart = "nix run git@github.com:billewanick/sealPostGenerator.git";
       };
     };
   };
